f1e2a9ec815ad57a690daf05e175e778a970f115
mikamo3 <kamo3proj@gmail.com>
2024-03-24 00:02:49 +0900

    Fix home

--- home.nix
+++ home.nix
@@ -1,8 +1,17 @@
+{ pkgs, ... }:
 {
   home = rec {
-    username="ユーザー名";
+    username="alice";
     homeDirectory = "/home/${username}";
     stateVersion = "22.11";
   };
   programs.home-manager.enable = true;
+  home.packages = with pkgs; [
+    bat
+    bottom
+    eza
+    httpie
+    pingu
+    ripgrep
+  ];
 }
