529af0e8fd94f26b85b677e6f1908e1c0ae4c5b0
mikamo3 <kamo3proj@gmail.com>
2024-03-23 23:44:39 +0900

    fix

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
 
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
