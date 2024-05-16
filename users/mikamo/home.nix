3ff34914fbac32016422eadbf53cf51f5150782c
mikamo3 <kamo3proj@gmail.com>
2024-05-17 00:28:52 +0900

    WIP

--- users/mikamo/home.nix
+++ users/mikamo/home.nix
@@ -10,22 +10,34 @@
     ./fontconfig.nix
     ./wezterm.nix
     ./i3.nix
+    ./autorandr.nix
   ];
   home = rec {
     username = "mikamo";
     homeDirectory = "/home/${username}";
     stateVersion = "23.11";
   };
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = [ "graphical-session-pre.target" ];
+    };
+  };
   programs.home-manager.enable = true;
   programs.powerline-go.enable = true;
-
+  xsession.enable = true;
   home.pointerCursor = with pkgs;{
     x11.enable = true;
     name = "volantes_cursors";
     package = volantes-cursors;
     size = 64;
   };
-
+  i18n.inputMethod.enabled = "fcitx5";
+  i18n.inputMethod.fcitx5.addons = with pkgs; [
+    fcitx5-mozc
+    fcitx5-gtk
+  ];
+  services.network-manager-applet.enable = true;
   xdg.enable = true;
   home.packages = with pkgs; [
   ];
